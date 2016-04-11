b075410363b3a4ff0fb4984dc1706237d8b56d0a
SongYeonSOO <lucklys324@gmail.com>
2016-04-11 16:09:09 +0900

    20160411

---
+++ WebContent/hello.js
@@ -0,0 +1,10 @@
+//document.write("<strong>Hello World</strong>");
+//alert("Hello World");
+document.write( "Hello World" );
+console.log("HEllo world");
+console.error("Error");
+console.warn("Warning");
+console.info("info");
+
+
+document.write( "Hello World" ); 
